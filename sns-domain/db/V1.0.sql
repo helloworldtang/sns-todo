2ad5bbdd364e612b70bb72277cb23b70cf3aa8f4
Tangcheng <helloworld.tang@qq.com>
2017-09-03 21:49:17 +0800

    update db script

--- sns-domain/db/V1.0.sql
+++ sns-domain/db/V1.0.sql
@@ -1,49 +1,25 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : localhost
-Source Server Version : 50505
+Source Server         : todolist
+Source Server Version : 50637
 Source Host           : localhost:3306
-Source Database       : sns-todo-dev
+Source Database       : sns-todo-prod
 
 Target Server Type    : MYSQL
-Target Server Version : 50505
+Target Server Version : 50637
 File Encoding         : 65001
 
-Date: 2017-08-31 22:53:55
+Date: 2017-09-03 20:39:42
 */
 CREATE DATABASE `sns-todo-dev`  DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
 
 SET FOREIGN_KEY_CHECKS=0;
 
--- ----------------------------
--- Table structure for sns_todo_detail
--- ----------------------------
-DROP TABLE IF EXISTS `sns_todo_detail`;
-CREATE TABLE `sns_todo_detail` (
-  `id` bigint(19) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` bigint(19) unsigned DEFAULT NULL,
-  `user_name` varchar(190) NOT NULL,
-  `category_id` bigint(19) NOT NULL,
-  `digest` varchar(200) DEFAULT NULL,
-  `content` text NOT NULL,
-  `weight` int(11) unsigned NOT NULL,
-  `finished` tinyint(1) NOT NULL,
-  `create_ip` varchar(50) DEFAULT NULL,
-  `update_ip` varchar(50) DEFAULT NULL,
-  `status` tinyint(2) NOT NULL,
-  `create_date` date NOT NULL,
-  `create_time` datetime NOT NULL,
-  `update_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 -- ----------------------------
--- Records of sns_todo_detail
--- ----------------------------
-
--- ----------------------------
--- Table structure for sns_todo_type
+-- Table structure for sns_todo_category
 -- ----------------------------
 DROP TABLE IF EXISTS `sns_todo_category`;
 CREATE TABLE `sns_todo_category` (
@@ -60,6 +36,26 @@ CREATE TABLE `sns_todo_category` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Records of sns_todo_type
+-- Table structure for sns_todo_detail
 -- ----------------------------
-
+DROP TABLE IF EXISTS `sns_todo_detail`;
+CREATE TABLE `sns_todo_detail` (
+  `id` bigint(19) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(19) unsigned DEFAULT NULL,
+  `user_name` varchar(190) NOT NULL,
+  `category_id` bigint(19) NOT NULL,
+  `digest` varchar(200) NOT NULL,
+  `content` text,
+  `weight` int(10) unsigned NOT NULL,
+  `expect_finish_time` datetime DEFAULT NULL,
+  `finished` tinyint(1) NOT NULL,
+  `create_ip` varchar(50) DEFAULT NULL,
+  `update_ip` varchar(50) DEFAULT NULL,
+  `status` tinyint(2) NOT NULL,
+  `create_date` date NOT NULL,
+  `create_time` datetime NOT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `finish_time` datetime DEFAULT NULL,
+  `finish_ip` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
