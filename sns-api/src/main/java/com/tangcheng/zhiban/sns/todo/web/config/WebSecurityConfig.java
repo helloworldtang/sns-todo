8fa37f9ce2c040c430a5f0338d675bf1f716f2b3
Tangcheng <helloworld.tang@qq.com>
2017-09-12 07:51:03 +0800

    MP support

--- sns-api/src/main/java/com/tangcheng/zhiban/sns/todo/web/config/WebSecurityConfig.java
+++ sns-api/src/main/java/com/tangcheng/zhiban/sns/todo/web/config/WebSecurityConfig.java
@@ -45,7 +45,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login")//用户在退出后将要被重定向到的URL。默认为/。将会通过HttpServletResponse.redirect来处理。
                 .and()
                 .authorizeRequests()
-                .antMatchers("/web/jars/**", "/favicon.ico", "/logo.png", "/css/**", "/js/**", "/login", "/signin/**", "/signup")
+                .antMatchers("/web/jars/**", "/favicon.ico", "/logo.png", "/css/**", "/js/**", "/login", "/mp/verify/**")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
