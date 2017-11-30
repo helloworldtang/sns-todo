1ed55e08360c2b3cc6cd250de1a56bed8a43af16
唐成 <tangcheng@hujiang.com>
2017-11-30 18:11:47 +0800

    output stdout 2 to nohup.out for back

--- sns-api/start.sh
+++ sns-api/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-java -Djava.security.egd=file:/dev/./urandom  -Dserver.port=80 -Dspring.profiles.active=prod -Xms600m -Xmx600m -jar sns-api-1.0.0.jar >/dev/null 2>&1 &
+# output stdout 2 to nohup.out for back
+nohup java -Djava.security.egd=file:/dev/./urandom  -Dserver.port=80 -Dspring.profiles.active=prod -Xms600m -Xmx600m -jar sns-api-1.0.0.jar >/dev/null &
