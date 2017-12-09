1e79a949f9a38da2bf30e575ef7a8c48d205414f
Tangcheng <helloworld.tang@qq.com>
2017-12-10 01:23:29 +0800

    1071-Specified key was too long;max key length is 767 bytes

--- sns-domain/db/v1.0.1.sql
+++ sns-domain/db/v1.0.1.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `sns_user`
 MODIFY COLUMN `type`  tinyint(2) UNSIGNED NOT NULL AFTER `id`,
-ADD COLUMN `third_part_id`  varchar(200) NULL AFTER `id`,
+ADD COLUMN `third_part_id`  varchar(150) NULL AFTER `id`,
 ADD COLUMN `nick_name`  varchar(200) NULL AFTER `type`,
 ADD INDEX `idx_third_part_id_type` (`third_part_id`, `type`) ;
 
