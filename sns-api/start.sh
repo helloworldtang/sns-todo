bad09c58aebbc923840c3feb5b004f140ff0a059
唐成 <tangcheng@hujiang.com>
2017-12-11 14:49:19 +0800

    optimize bash script

--- sns-api/start.sh
+++ sns-api/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # output stdout 2 to nohup.out for back
-nohup java -Djava.security.egd=file:/dev/./urandom  -Dserver.port=80 -Dspring.profiles.active=prod -Xms600m -Xmx600m -jar sns-api-1.0.0.jar >/dev/null &
+AppName=sns-api-1.0.1.jar
+nohup java -Djava.security.egd=file:/dev/./urandom  -Dserver.port=80 -Dspring.profiles.active=prod -Xms600m -Xmx600m -jar  ${AppName} >/dev/null &
