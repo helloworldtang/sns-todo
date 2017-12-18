717429f7dda07586caa9205b860962aedc2288dd
唐成 <tangcheng@hujiang.com>
2017-12-18 16:00:45 +0800

    refactor swagger support

--- sns-domain/src/main/java/com/tangcheng/zhiban/sns/todo/domain/req/TodoDetailReqVO.java
+++ sns-domain/src/main/java/com/tangcheng/zhiban/sns/todo/domain/req/TodoDetailReqVO.java
@@ -1,26 +1,33 @@
 package com.tangcheng.zhiban.sns.todo.domain.req;
 
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
  * Created by tangcheng on 8/26/2017.
+ *
+ * @ApiModelProperty value值会显示在swagger Parameters 中的第三列Description
+ * 效果如下图所示
+ * http://images2017.cnblogs.com/blog/280044/201712/280044-20171218155806693-1915989979.png
  */
 public class TodoDetailReqVO {
 
-    private Long categoryId=1L;
+    private Long categoryId = 1L;
 
     @Min(1)
-    private Integer weight=1;
+    @ApiModelProperty(required = true, value = "权重")
+    private Integer weight = 1;
 
     @NotEmpty
     @Size(min = 2, max = 200)
+    @ApiModelProperty(required = true, value = "摘要")
     private String digest;
 
     @Size(max = 1000)
+    @ApiModelProperty(required = true, value = "详细内容")
     private String content;
 
     public Long getCategoryId() {
